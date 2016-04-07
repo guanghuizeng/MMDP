fcf4e799463b72a42388be4558994effaf2a6a90
Guanghui Zeng <guanghui.zeng@live.com>
2016-04-08 03:10:18 +0800

    修改 Sync 的操作逻辑

--- mmdp/src/main/java/io/guanghuizeng/fs/sync/SyncClient.java
+++ mmdp/src/main/java/io/guanghuizeng/fs/sync/SyncClient.java
@@ -1,25 +1,50 @@
 package io.guanghuizeng.fs.sync;
 
 import io.guanghuizeng.fs.AbsoluteFilePath;
-import io.netty.buffer.ByteBuf;
 
-/**
- * Created by guanghuizeng on 16/4/6.
- */
+/*************************
+ * SyncClient
+ * <p>
+ * 与 SyncServer 交互
+ *************************/
 public class SyncClient {
+
+    private SyncClientHandler handler = new SyncClientHandler();
 
     public SyncClient(AbsoluteFilePath afp) {
 
     }
-    public void push(ByteBuf cache) {
 
+    /*****************
+     *      API
+     *****************/
+
+    /**
+     * 将数据保存到 server
+     *
+     * @param buffer 数据
+     */
+    public void push(SyncBuffer buffer) {
     }
 
-    public ByteBuf poll(int size) {
+    /**
+     * 从 server 获取数据
+     *
+     * @param path     路径
+     * @param mode     权限
+     * @param position 相对位置
+     * @param length   要读取的数据量
+     * @return 数据
+     */
+    public SyncBuffer poll(String path, String mode, int position, int length) {
         return null;
     }
 
-    // 文件大小
+    /**
+     * 文件大小
+     *
+     * @return 文件大小
+     */
     public long getLength() {
         return 0;
     }
