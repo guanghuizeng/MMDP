e4634aadfe99a8a8d7ec6ab1f598acd5c4cb9f86
Guanghui Zeng <guanghui.zeng@live.com>
2016-04-08 03:08:37 +0800

    使用新的缓存实现、修改同步的逻辑、修改注释
    反思：一次就该做一件事情！像这样混杂在一起，就会导致逻辑混乱！非常不爽！！！

--- mmdp/src/main/java/io/guanghuizeng/fs/sync/SyncService.java
+++ mmdp/src/main/java/io/guanghuizeng/fs/sync/SyncService.java
@@ -2,21 +2,21 @@ package io.guanghuizeng.fs.sync;
 
 import io.guanghuizeng.fs.AbsoluteFilePath;
 import io.guanghuizeng.fs.FileSystem;
+import io.guanghuizeng.fs.sync.protocol.SyncMessage;
 import io.netty.buffer.ByteBuf;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+/*************************
  * 实现文件同步
- */
+ *************************/
 public class SyncService {
 
     /**
-     * 用 ByteBuf 设置缓存
+     * 设置缓存
      */
-    private int maxCapacity = 1024;
-    private ByteBuf cache;
+    private SyncBuffer buffer;
 
     /**
      * 记录文件总体的length, 是各文件的总和
@@ -30,7 +30,11 @@ public class SyncService {
     private List<AbsoluteFilePath> absoluteFilePathList;
     private SyncClient presentClient;
 
-    public SyncService(FileSystem fileSystem, String path, int cacheCapacity) {
+    /*************************
+     * Constructors
+     *************************/
+
+    public SyncService(FileSystem fileSystem, String path) {
 
         // 解析 path 到 ABF; 添加 SyncClients 到 cluster
         absoluteFilePathList = fileSystem.resolve(path);
@@ -38,27 +42,23 @@ public class SyncService {
             cluster.add(new SyncClient(afp));
         }
 
-        // 初始化 length, presentClient, cache
+        // 初始化 length, presentClient, buffer
         for (SyncClient client : cluster) {
             length = length + client.getLength();
         }
         if (!cluster.isEmpty()) {
             presentClient = cluster.get(0);
         }
-        maxCapacity = cacheCapacity;
-        cache = presentClient.poll(maxCapacity);
     }
 
-    public SyncService(FileSystem fileSystem, AbsoluteFilePath afp, int cacheCapacity) {
+    public SyncService(FileSystem fileSystem, AbsoluteFilePath afp) {
 
         // 添加 SyncClient 到 cluster
         cluster.add(new SyncClient(afp));
 
-        // 初始化 length, presentClient, cache
+        // 初始化 length, presentClient, buffer
         length = cluster.get(0).getLength();
         presentClient = cluster.get(0);
-        maxCapacity = cacheCapacity;
-        cache = presentClient.poll(maxCapacity);
     }
 
     /*************************
@@ -77,45 +77,53 @@ public class SyncService {
      *
      * @param buffer
      */
-    public SyncService sync(final ByteBuf buffer) {
+    public SyncService sync(final SyncBuffer buffer) {
         /**
-         * TODO: 改进. 若是能够记录是否被改动过, 效率会高很多.
+         * TODO: 改进. 若是能够记录是否被改动过, 效率会高很多. 重新设计缓存, 可以实现这一点
          */
-        if (!cache.equals(buffer)) {
-            cache = buffer.copy();  // 更新cache
-            push();                 // push cache 到服务器
+        if (!this.buffer.equals(buffer)) {
+            this.buffer = buffer.copy();        // 更新 buffer
+            presentClient.push(buffer.copy());  // 创建一个新的对象, 用来传递数据
+            // TODO: 调整 presentClient
         }
         return this;
     }
 
     /**
-     * 向 buffer 中写入数据
+     * 更新 buffer: 从 server 获取数据, 写入到buffer
      *
-     * @param buffer 以ByteBuf类型为参数, 且将数据写入到其中,
+     * @param buffer 以SyncBuffer类型为参数, 且将数据写入到其中,
      *               可以根据buffer的size来决定写入多少数据! 更灵活!
      * @return
      */
-    public SyncService next(ByteBuf buffer) {
-        poll();                   // 先更新 cache
+    public SyncService next(SyncBuffer buffer) {
+
+        /**
+         * - presentClient.pool()
+         * - 检查是否要跳到下一个 client
+         *
+         * 每次push和poll数据后, 可能要调整本地的记录, 如 presentClient
+         * buffer 指向一个新的 ByteBuf 对象; 原先的对象将被 GC 处理
+         */
+
+        /**
+         * 调整 presentClient
+         */
+
+        if (presentClient == null) {
+            // EOF exception
+        }
+
+        // 设置 position, 读取足够多的数据
+        this.buffer = presentClient.poll(buffer.getPath(), buffer.getMode(),
+                buffer.getPosition() + buffer.capacity(), buffer.getLength());
+        // TODO: 调整 presentClient
+
         buffer.clear();
-        buffer.writeBytes(cache); // 将 cache 中的数据写入 buffer
+        buffer.writeBytes(this.buffer); // 将 buffer 中的数据写入 buffer
         return this;
     }
 
-    /**
-     * 将 cache 发送到 server
-     */
-    private void push() {
-        presentClient.push(cache.copy());  // 创建一个新的对象, 用来传递数据
-    }
-
-    /**
-     * 更新 cache: 从 server 获取数据, 写入到cache
-     */
-    private void poll() {
-        // cache 指向一个新的 ByteBuf 对象; 原先的对象将被 GC 处理
-        cache = presentClient.poll(cache.capacity());
-    }
 
     /**
      * 获取文件大小
