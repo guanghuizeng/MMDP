5c87975030ccffd10ee5f992082eaf53b5b42d93
Guanghui Zeng <guanghui.zeng@live.com>
2016-04-22 17:29:22 +0800

    测试用例

--- mmdp/src/test/java/io/guanghuizeng/mmdp/engine/EngineTest.java
+++ mmdp/src/test/java/io/guanghuizeng/mmdp/engine/EngineTest.java
@@ -1,7 +1,22 @@
 package io.guanghuizeng.mmdp.engine;
 
+import io.guanghuizeng.fs.VirtualPath;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
 /**
  * Created by guanghuizeng on 16/4/22.
  */
 public class EngineTest {
+
+    @Test
+    public void test() {
+
+        VirtualPath in = new VirtualPath(Paths.get("/data/data1k64"));
+        VirtualPath out = new VirtualPath(Paths.get("/data/data1k64Sorted"));
+
+        Engine engine = new Engine();
+        engine.sort(in, out);
+    }
 }
