6dbfc11a69971a38667637da55945c82000f3758
Guanghui Zeng <guanghui.zeng@live.com>
2016-04-22 17:32:11 +0800

    完善路径设计

--- mmdp/src/main/java/io/guanghuizeng/fs/VirtualPath.java
+++ mmdp/src/main/java/io/guanghuizeng/fs/VirtualPath.java
@@ -1,7 +1,24 @@
 package io.guanghuizeng.fs;
 
+import java.nio.file.Path;
+
 /**
  * Created by guanghuizeng on 16/4/22.
  */
 public class VirtualPath {
+
+    private String prefix = "fs://";
+    private Path localPath;
+
+    public VirtualPath(Path localPath) {
+        this.localPath = localPath;
+    }
+
+    public String toString() {
+        return prefix.concat(localPath.toString());
+    }
+
+    public Path getLocalPath() {
+        return localPath;
+    }
 }
--- mmdp/src/main/java/io/guanghuizeng/fs/VirtualUrl.java
+++ mmdp/src/main/java/io/guanghuizeng/fs/VirtualUrl.java
@@ -1,7 +1,36 @@
 package io.guanghuizeng.fs;
 
+import java.nio.file.Path;
+
 /**
  * Created by guanghuizeng on 16/4/22.
  */
 public class VirtualUrl {
+
+    private String prefix = "fs://";
+    private String infix = ":";
+    private Address address = new Address();
+    private Path localPath;
+
+    public VirtualUrl(Address address, Path localPath) {
+        this.address.copy(address);
+        this.localPath = localPath;
+    }
+
+    public Path getLocalPath() {
+        return localPath;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public String toString() {
+        return prefix.concat(address.getHost())
+                .concat(infix)
+                .concat(String.valueOf(address.getSyncPort()))
+                .concat(infix)
+                .concat(String.valueOf(address.getEnginePort()))
+                .concat(localPath.toString());
+    }
 }
