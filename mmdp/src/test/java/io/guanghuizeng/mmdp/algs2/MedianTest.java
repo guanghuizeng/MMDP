6270777e9df3fd642c1586fd8ffa098e6c9af27c
Guanghui Zeng <guanghui.zeng@live.com>
2016-05-02 23:54:05 +0800

    新增中位数查找的测试
    注意：有些测试项不是完备的单元测试。

--- mmdp/src/test/java/io/guanghuizeng/mmdp/algs2/MedianTest.java
+++ mmdp/src/test/java/io/guanghuizeng/mmdp/algs2/MedianTest.java
@@ -1,16 +1,18 @@
 package io.guanghuizeng.mmdp.algs2;
 
 import io.guanghuizeng.fs.FileSystem;
-import io.guanghuizeng.mmdp.algs.MedianFinder64;
+import io.guanghuizeng.fs.ServiceID;
+import io.guanghuizeng.fs.Uri;
+import io.guanghuizeng.fs.VirtualPath;
 import io.guanghuizeng.mmdp.Engine;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -19,47 +21,12 @@ import java.util.Random;
 
 public class MedianTest {
 
-
-    @Test
-    public void test0() throws IOException {
-
-        File file = File.createTempFile("median", "test");
-        file.deleteOnExit();
-        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
-
-        Random random = new Random();
-
-        long start = random.nextLong();
-        long N = 10 * 1000;
-
-        for (long i = start; i < start + N; i++) {
-            stream.writeLong(i);
-        }
-        stream.close();
-
-        long expected = (start * 2 + N - 1) / 2;
-
-        MedianFinder64 finder = new MedianFinder64();
-        long result = finder.find(file, N);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    public void test01() throws IOException {
-        for (int i = 0; i < 50; i++) {
-            test0();
-        }
-    }
-
     @Test
     public void test3() throws IOException {
 
-        // unsigned
-
         File file = File.createTempFile("median", "test");
         file.deleteOnExit();
-        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(file));
+        FileOutputBuffer buffer = new FileOutputBuffer(file.toPath());
 
         ArrayList<Long> data = new ArrayList<>();
 
@@ -67,16 +34,16 @@ public class MedianTest {
         int N = 10000;
         for (long i = 0; i < N; i++) {
             long n = Math.abs(random.nextLong());
-            stream.writeLong(n);
+            buffer.writeLong(n);
             data.add(n);
         }
-        stream.close();
+        buffer.close();
 
         data.sort(Comparator.naturalOrder());
         Long expected = data.get((N - 1) / 2);
 
-        Engine engine = new Engine(new FileSystem());
-        long result = engine.median(Paths.get(file.getPath()), N);
+        MedianFinder64 finder64 = new MedianFinder64();
+        long result = finder64.find(file.toPath(), N);
 
         assertEquals(Long.toUnsignedString(expected), Long.toUnsignedString(result));
     }
@@ -86,5 +53,44 @@ public class MedianTest {
         for (int i = 0; i < 20; i++) {
             test3();
         }
+    }
+    
+    public void test6() throws IOException {
+
+        /** CAUTION: 该测试对文件系统有前置条件, 不能算是unit test. */
+
+        String input = "/data/data10m64Test";
+        Path dir = Paths.get(System.getProperty("user.home"), "/mmdpfs/user0", input);
+
+        File file = Files.createFile(dir).toFile();
+        file.deleteOnExit();
+        FileOutputBuffer buffer = new FileOutputBuffer(file.toPath());
+
+        ArrayList<Long> data = new ArrayList<>();
+
+        Random random = new Random();
+        int N = 10 * 1000;
+        for (long i = 0; i < N; i++) {
+            long n = Math.abs(random.nextLong());
+            buffer.writeLong(n);
+            data.add(n);
+        }
+        buffer.close();
+
+        /** 排序列表元素, 再取出中位数 */
+        data.sort(Comparator.naturalOrder());
+        Long expected = data.get((N - 1) / 2);
+
+        /** 用 Engine.median 方法计算中位数 */
+        VirtualPath in = new VirtualPath(Paths.get(input));
+        FileSystem fileSystem = new FileSystem();
+        ServiceID service0 = new ServiceID("127.0.0.1", 8070, 8090);
+        fileSystem.addServices(service0);
+        fileSystem.put(in, new Uri(service0, new VirtualPath(input)));
+
+        Engine engine = new Engine(fileSystem);
+        Long result = engine.median(in, N);
+
+        assertEquals(expected, result);
     }
 }
